f0738fe75b58ba9fd7829fd0336e2deba78d31fa
unknown <helene.carlier.gubler@gmail.com>
2019-11-28 15:37:17 +0100

    creation schema sensor

--- api/models/schemas/sensorSchema.js
+++ api/models/schemas/sensorSchema.js
@@ -1,1 +1,17 @@
-sensorSchema.js
+const mongoose = require("mongoose");
+
+const sensorSchema = new mongoose.Schema(
+    {
+        location: {
+            type: String,
+            required: true,
+        }
+    },{
+        timestamps : 
+        {
+            createdAt : "creationDate"
+        }
+    }
+    );
+
+module.exports = mongoose.model('user', userSchema);
