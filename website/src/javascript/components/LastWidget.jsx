3bcff2366a24a7739218868f5ead6233246f875c
Ikka <daniel.ikka@edu.ece.fr>
2019-12-04 15:29:50 +0100

    last_widget created and implement

---
+++ website/src/javascript/components/LastWidget.jsx
@@ -0,0 +1,53 @@
+import React , {Component} from 'react'
+import {Col} from "react-bootstrap";
+
+class LastWidget extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.styles = {
+            light: {
+                fontWeight: 100,
+                backgroundColor: props.lightBG ? props.lightBG : "rgb(218,83,103,1)",
+                color: props.lightCol? props.lightCol : "white",
+                borderRadius: '0.4em',
+                border: 'none',
+
+                transition : 'color 500ms, background-color 500ms'
+            },
+            dark: {
+                fontWeight: 100,
+                backgroundColor: props.darkBG ? props.darkBG : "rgb(218,83,103,0.5)",
+                color: props.darkCol ? props.darkCol : "white",
+                borderRadius: '0.4em',
+                border: 'none',
+
+                transition : 'color 500ms, background-color 500ms'
+            }
+        }
+    }
+
+    render() {
+        //Content
+        const { mode, value, iconName} = this.props;
+
+        //responsive
+        const {xs, sm, md, xl, lg} = this.props;
+
+        return (
+            <Col xs={xs} sm={sm} md={md} xl={xl} lg={lg}
+                 style={mode ? this.styles.dark : this.styles.light}
+                 className={"p-3"}
+            >
+                <h1 className={"t-size-1-5 fw-600"}>Last Measure</h1>
+                <h2 className={"t-size-5 fw-100 font-italic text-center m-0"}>
+                    <i className={'material-icons t-size-4'}>{iconName}</i>
+                    {value}
+                </h2>
+            </Col>
+        );
+    }
+}
+
+export default LastWidget;
--- website/src/javascript/pages/Dashboard.jsx
+++ website/src/javascript/pages/Dashboard.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import Header from '../components/Header'
-import {Col, Row} from "react-bootstrap";
+import {Col, Container} from "react-bootstrap";
 import CustomBtn from "../components/CustomBtn"
+import LastWidget from "../components/LastWidget"
 
 
 const styles = {
@@ -40,13 +41,15 @@ class Dashboard extends Component {
         return (
             <div id={'main-wrapper'} style={mode ? styles.dark : styles.light}>
                 <Header mode={mode} onSwitch={onSwitch} onRefresh={this.handleRefresh} onSearch={this.onSearch}/>
-                <Row className={'dashboard-content'}>
-                    <Col className={"mx-5 d-flex"}>
-                        <Col xs={9} className={"my-5"}>
-                            <h1 className={"fw-700 mb-0"}>Dashboard</h1>
-                            <h2 className={"fw-200 mt-0"}>Welcome to the Domotics Management Panel</h2>
+                <Container fluid={true} id={'dashboard-wrapper'} className={'dashboard-content'}>
+                    <Col id={"header-dashB-wrapper"} className={"d-flex"}>
+
+                        <Col id={'titles'} xs={9} className={"my-5 p-0"}>
+                            <h1 className={"fw-800 mb-0"}>Dashboard</h1>
+                            <h2 className={"fw-200 mt-0 t-size-1-3"}>Welcome to the Domotics Management Panel</h2>
                         </Col>
-                        <Col className={"d-flex justify-content-end align-items-center px-0"}>
+
+                        <Col id={'btn'} className={"d-flex justify-content-end align-items-center px-0"}>
                             <CustomBtn xs={8} sm={6} md={9} lg={8} xl={6}
                                        mode={mode} icon={"settings"} text={"Settings"}
                                        onClick={this.handleSettings}
@@ -55,8 +58,17 @@ class Dashboard extends Component {
                             </CustomBtn>
                         </Col>
                     </Col>
-
-                </Row>
+                    <Col>
+                        <LastWidget xs={6} md={4}
+                                    mode={mode} value={"35%"} iconName={"invert_colors"}
+                                    darkBG={'rgb(218,83,103,0.5)'} lightBG={'rgb(218,83,103,1)'}
+                        />
+                        <LastWidget xs={6} md={4}
+                                    mode={mode} value={"35%"} iconName={"invert_colors"}
+                                    darkBG={'rgb(229,183,82,0.5)'} lightBG={'rgb(229,183,82,1)'}
+                        />
+                    </Col>
+                </Container>
             </div>
         );
     }
