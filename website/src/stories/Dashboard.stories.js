0cdbcea71619a01db40ac0b1cd0abb601de99f2d
unknown <helene.carlier.gubler@gmail.com>
2019-12-08 16:53:30 +0100

    Dashboard

---
+++ website/src/stories/Dashboard.stories.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import {storiesOf} from "@storybook/react"
+import {action} from "@storybook/addon-actions"
+import Dashboard from "../javascript/pages/Dashboard"
+import index from "../index"
+
+storiesOf('Dashboard', module)
+  .add('Dark Mode', () => 
+  <div>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
+    <Dashboard mode={true}
+        onSwitch={action("onSwitch")}
+        onRefresh={action("onRefresh")}
+    />
+    </div>)
+  .add('Light Mode', () => 
+  <div>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
+    <Dashboard mode={false}
+        onSwitch={action("onSwitch")}
+        onRefresh={action("onRefresh")}
+    />
+  </div>)
--- website/src/stories/Header.stories.js
+++ website/src/stories/Header.stories.js
@@ -4,7 +4,7 @@ import {action} from "@storybook/addon-actions"
 import Header from "../javascript/components/Header"
 import index from "../index"
 
-storiesOf('Header', module)
+storiesOf('Components/Header', module)
   .add('Light Mode', () => 
   <div>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
