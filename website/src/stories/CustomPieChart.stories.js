c5d25e4ff21a70aea1d675e391a27bec615c4a83
unknown <helene.carlier.gubler@gmail.com>
2019-12-09 22:51:11 +0100

    ca ne fonctionne pas :-1:

--- website/src/stories/CustomPieChart.stories.js
+++ website/src/stories/CustomPieChart.stories.js
@@ -6,35 +6,30 @@ import index from "../index"
 
 storiesOf('Components/CustomPieChart', module)
   .add('Dark mode waiting', () => <CustomPieChart 
-    data = [
-    {name : "france", value : 25, number: 25},
+    data = {{name : "france", value : 25, number: 25},
     {name : "UK", value : 25, number: 25},
     {name : "USA", value : 25, number: 25},
-    {name : "Egypte", value : 25, number: 25}
-    ] 
+    {name : "Egypte", value : 25, number: 25}}
     mode={true} waiting={true}/>)
 
   .add('Dark mode', () => <CustomPieChart 
-  data = [
+  data = {
     {name : "france", value : 25, number: 25},
     {name : "UK", value : 25, number: 25},
     {name : "USA", value : 25, number: 25},
-    {name : "Egypte", value : 25, number: 25}
-    ]
+    {name : "Egypte", value : 25, number: 25}}
   mode={true} waiting={false}/>)
   .add('Light mode waiting', () => <CustomPieChart 
-  data = [
+  data = {
     {name : "france", value : 25, number: 25},
     {name : "UK", value : 25, number: 25},
     {name : "USA", value : 25, number: 25},
-    {name : "Egypte", value : 25, number: 25}
-    ]
+    {name : "Egypte", value : 25, number: 25}}
      mode={false} waiting={true}/>)
   .add('Light mode', () => <CustomPieChart 
-  data = [
+  data = {
     {name : "france", value : 25, number: 25},
     {name : "UK", value : 25, number: 25},
     {name : "USA", value : 25, number: 25},
-    {name : "Egypte", value : 25, number: 25}
-    ]
+    {name : "Egypte", value : 25, number: 25}}
      mode={false} waiting={false}/>)
